8ab4d97161bc47722809a42f9e8d6419d6bab641
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-16 23:01:59 +0100

    main

--- typescript-gm-app/src/main.tsx
+++ typescript-gm-app/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/typescript-gm-app">
+    <BrowserRouter basename="typescript-gm-app">
      <App />
     </BrowserRouter>
    
