4494a755b6d9efca771631782d898d3a40d8c9d5
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-19 15:42:08 +0100

    updated our classes

--- src/scenes/OurClasses/OurClassesList.tsx
+++ src/scenes/OurClasses/OurClassesList.tsx
@@ -52,7 +52,7 @@ const OurClassesList = ({ setSelectedPage }: Props) => {
   return (
     <section id="ourclasses" className="w-full bg-primary-100 py-40">
       <motion.div
-        onViewportEnter={() => setSelectedPage(setSelectedPage.OurClasses)}
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
         <motion.div
           className="mx-auto w-5/6"
