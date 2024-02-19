3951b57573ab613bd867e9d7e3cbac825c5bc232
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-19 09:35:03 +0100

    Benefits list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/Navbar/Navbar";
 import Home from "@/scenes/Home/Home";
+import Benefits from "@/scenes/Benefits/BenefitsList";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -29,6 +30,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage}/>
+      <Benefits setSelectedPage={setSelectedPage}/>
     </div>
   );
 }
